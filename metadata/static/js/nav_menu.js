f816ca4e9d3e27ee0b54a78fe0114435c1a10dc1
arrk <arrkoz1@gmail.com>
2016-10-15 17:56:33 +1100

    ok

--- metadata/static/js/nav_menu.js
+++ metadata/static/js/nav_menu.js
@@ -25,9 +25,9 @@ myapp.controller('menuController', function($scope,$http) {
 });
 // end of menuController
 
-// start of ldmController
+// start of metadataObjController
 myapp.controller('metadataObjController', function($scope,$http,$filter,$location,$log) {
-
+    
     console.log("Inside the metadataObjController");
     
     // Default Value for System Name
@@ -38,9 +38,19 @@ myapp.controller('metadataObjController', function($scope,$http,$filter,$locatio
     // Default Value for Instance Name
     //    $scope.instance_name = "LDM";
     
+//    console.log("CK value= "+$scope.ck);
+//    $scope.ck = false;
+//    console.log("CK value= "+$scope.ck);
+//    
+//    $scope.$watch('f["Entity_Name"]',function(value) {
+//       console.log("ck: "+value);
+//        $scope.ck = false ; 
+//    });
+    
     $scope.$watch('system_name', function(value) {
        console.log("system_name: "+value);
         $scope.system_name = value ; 
+        
     // }); // end of watch
     
 
@@ -64,6 +74,7 @@ myapp.controller('metadataObjController', function($scope,$http,$filter,$locatio
 
                 // strict comparison flat = true, to do an exact match
                 $scope.rows = $filter('filter')(data,  {"System_Name": $scope.system_name},true);
+                $scope.rows = $filter('orderBy')($scope.rows,  "System_Name",true);
 
                 $scope.count_display_flag = true;
                 $scope.rows_count = ($scope.rows).length;
@@ -77,7 +88,119 @@ myapp.controller('metadataObjController', function($scope,$http,$filter,$locatio
 
             }); // end of http.get.success
         }); // end of watch
-    }); // end of ldmController
+    }); // end of metadataObjController
+
+
+// start of metadataMappingController
+myapp.controller('metadataMappingController', function($scope,$http,$filter,$location,$log) {
+
+    console.log("Inside the metadataMappingController");
+    
+    // Default Value for Source System Name
+    // $scope.system_name = "ABS" ; 
+     $scope.source_system_name = "UX Sitemap" ; 
+    // $scope.system_name = "" ; 
+    
+    // Default Value for Source Instance Name
+    //    $scope.source_instance_name = "LDM";
+    
+    $scope.$watch('source_system_name', function(value) {
+       console.log("source_system_name: "+value);
+        $scope.source_system_name = value ; 
+    // }); // end of watch
+    
+
+            $log.warn("Before HTTP GET | source_system_name: "+$scope.source_system_name+ " source_instance_name: "+$scope.source_instance_name);
+
+            url_hash  = $location.hash()
+            url_path  = $location.path()
+            url_absUrl = $location.absUrl()
+
+            $log.info("url_hash"+url_hash);$log.info("url_path"+url_path);$log.info("url_absUrl"+url_absUrl);
+    
+    
+            $http.get('/static/json/BTP_Phase1_2_Mappings.json').success(function(data){
+
+                // $scope.rows = data;
+                $log.warn("After HTTP GET | source_system_name: "+$scope.source_system_name + " source_instance_name: "+$scope.source_instance_name);            
+
+                // Filter the rows from the json based on the page context , parameters initialized in the div ng-controller
+
+                //$scope.rows = $filter('filter')(data, { "System Name": $scope.system_name,"Instance Name":$scope.instance_name });
+
+                // strict comparison flat = true, to do an exact match
+                $scope.rows = $filter('filter')(data,   {"Source_System_Name": $scope.source_system_name},true);
+                $scope.rows = $filter('orderBy')($scope.rows,  "Source_System_Name",true);
+
+                $scope.count_display_flag = true;
+                $scope.rows_count = ($scope.rows).length;
+                console.log("Length: "+ ($scope.rows).length + ", Size: " + $scope.rows.size)
+
+                console.log("Display flag and count = "+ $scope.count_display_flag + " and " + $scope.rows_count );
+
+                console.log("I'm called, ldm, http get success");
+                //console.log($scope.rows);
+                console.log("Display flag and count = "+ $scope.count_display_flag + " and " + ($scope.rows_count) );
+
+            }); // end of http.get.success
+        }); // end of watch
+    }); // end of metadataMappingController
+
+
+// start of metadataLineageController
+myapp.controller('metadataLineageController', function($scope,$http,$filter,$location,$log) {
+
+    console.log("Inside the metadataLineageController");
+    
+    // Default Value for Source System Name
+    // $scope.system_name = "ABS" ; 
+     //$scope.screen_entity_name = "UX Sitemap" ; 
+    // $scope.system_name = "" ; 
+    
+    // Default Value for Source Instance Name
+    //    $scope.source_instance_name = "LDM";
+    
+    $scope.$watch('screen_entity_name', function(value) {
+       console.log("screen_entity_name: "+value);
+        $scope.screen_entity_name = value ; 
+     }); // end of watch
+    
+
+            $log.warn("Before HTTP GET | source_system_name: "+$scope.source_system_name+ " source_instance_name: "+$scope.source_instance_name);
+
+            url_hash  = $location.hash()
+            url_path  = $location.path()
+            url_absUrl = $location.absUrl()
+
+            $log.info("url_hash"+url_hash);$log.info("url_path"+url_path);$log.info("url_absUrl"+url_absUrl);
+    
+    
+            $http.get('/static/json/BTP_Phase1_2_Lineage.json').success(function(data){
+
+                // $scope.rows = data;
+                $log.warn("After HTTP GET | source_system_name: "+$scope.source_system_name + " source_instance_name: "+$scope.source_instance_name);            
+
+                // Filter the rows from the json based on the page context , parameters initialized in the div ng-controller
+
+                //$scope.rows = $filter('filter')(data, { "System Name": $scope.system_name,"Instance Name":$scope.instance_name });
+
+                // strict comparison flat = true, to do an exact match
+                $scope.rows = $filter('filter')(data,   {"Screen_Entity_Name": $scope.screen_entity_name},true);
+                //$scope.rows = $filter('orderBy')($scope.rows,  "Screen_Entity_Name",true);
+
+                $scope.count_display_flag = true;
+                $scope.rows_count = ($scope.rows).length;
+                console.log("Length: "+ ($scope.rows).length + ", Size: " + $scope.rows.size)
+
+                console.log("Display flag and count = "+ $scope.count_display_flag + " and " + $scope.rows_count );
+
+                console.log("I'm called, ldm, http get success");
+                //console.log($scope.rows);
+                console.log("Display flag and count = "+ $scope.count_display_flag + " and " + ($scope.rows_count) );
+
+            }); // end of http.get.success
+        // }); // end of watch
+    }); // end of metadataLineageController
 
 
 myapp.controller('TableFilterController', function($scope,$http,$filter) {
